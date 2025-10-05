3fe3a7d0d4f7ce5d8d478d70f2b6aec5fb4a4908
Arman Jasuja <arman_jasuja@yahoo.com>
2025-10-05 17:33:36 +0400

    Add stack overflow illustration

--- 2_chapter/stack_smash.c
+++ 2_chapter/stack_smash.c
@@ -1,7 +1,9 @@
+// Program that illustrates a stack overflow
 #include <stdio.h>
 
 void strjoin(char *a, const char *b)
 {
+    // The key issue is that a needs to be large enough to accomodate both a and b
     while (*(a++)) 
         ;
     char *str = a - 1;
@@ -15,7 +17,7 @@ int main(void)
 {
     char a[10];
     char b[10];
-    scanf("%s", a);
+    scanf("%s", a); // There is a risk here of overflowing a, depending on how large the input is
     scanf("%s", b);
     strjoin(a, b);
     printf("Result is %s", a);
